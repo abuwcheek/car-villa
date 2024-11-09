2cbd006ed6c638e097840fdb8641d1d83d674eb1
abuwcheek <abdulloistamov1034@gmail.com>
2024-11-09 21:40:21 +0500

    category, brand, detail qo'shildi

--- car/urls.py
+++ car/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, brandView, categoryView, detailView
 
 
 urlpatterns = [
      path('', IndexView, name='index'),
+     path('brand/<uuid:uuid>/', brandView, name='brandview'),
+     path('category/<uuid:uuid>/', categoryView, name='categoryview'),
+     path('car/<uuid:uuid>/', detailView, name='detailview'),
 ]
--- car/views.py
+++ car/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from .models import Category, Brands, CarVilla, Testimonals, About
 
@@ -10,6 +10,7 @@ def IndexView(request):
      cars = CarVilla.objects.filter(is_active=True).all()
      featured_cars = CarVilla.objects.filter(is_featured=True)[:4]
      latest_cars = CarVilla.objects.filter(is_published=True)[:4]
+
 
      context = {
           'brands': brands,
@@ -34,11 +35,38 @@ def category_list(request):
 
 
 
-# def testimonals(request):
-#      testimonal = Testimonals.objects.all()[:3]
+def brandView(request, uuid):
+     brand = get_object_or_404(Brands, id=uuid)
+     brand_cars = brand.brand_car.all()
 
 
-#      context = {
-#           'testimonal': testimonal,
-#      }
-#      return render(request, 'index.html', context)
+     context = {
+          'brand': brand,
+          'brand_cars': brand_cars,
+     }
+     return render(request, 'brand_cars.html', context)
+
+
+
+def categoryView(request, uuid, slug):
+     category = get_object_or_404(Category, id=uuid)
+     category_cars = category.category_car.all()
+
+     context = {
+          'category': category,
+          'category_cars': category_cars,
+     }
+
+     return render(request, 'category_cars.html', context)
+
+
+
+
+def detailView(request, uuid):
+     detail_product = get_object_or_404(CarVilla, id=uuid)
+
+     context = {
+          'detail_product': detail_product,
+     }
+     
+     return render(request, 'detail_product.html', context)
